75290145ddd3270b542cdf3e4afff18b6851d862
karthikeh02 <karthiraj453@gmail.com>
2024-10-23 08:16:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Deposit collateral ETH/WETH
+2. Borrow another asset: DAI (stable coin)
+3. Repay the DAI
---
+++ hardhat.config.js
@@ -0,0 +1,82 @@
+const { version } = require("chai")
+
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+// require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+
+module.exports = {
+    // solidity: "0.8.24",
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.24",
+            },
+            {
+                version: "0.4.19",
+            },
+            {
+                version: "0.6.12",
+            },
+        ],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: { url: MAINNET_RPC_URL },
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        sepolia: {
+            chainId: 11155111,
+            blockConfirmations: 6,
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+        customChains: [
+            {
+                network: "sepolia",
+                chainId: 11155111,
+                urls: {
+                    apiURL: "https://api-sepolia.etherscan.io/api",
+                    browserURL: "https://sepolia.etherscan.io",
+                },
+            },
+        ],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 300000, // 500 seconds max
+    },
+}
